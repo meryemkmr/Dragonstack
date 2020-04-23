160a503a82b6607712a32a240b5f6047f70b611b
Meryem Kömür <meryemkmr36@gmail.com>
2020-04-23 13:09:54 -0500

    addeded

---
+++ server/config.js
@@ -0,0 +1,16 @@
+const SECONDS = 1000;
+const MINUTES = SECONDS * 60
+const HOURS = MINUTES * 60
+const DAY = HOURS * 24
+
+const RESRESH_RATE = 5 //units
+
+
+module.exports = {
+    SECONDS,
+    MINUTES,
+    HOURS,
+    DAY,
+    RESRESH_RATE
+}
+
